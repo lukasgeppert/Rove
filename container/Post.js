c61efc766c19db337cdc1e7fbb2ccc40587ec7bb
Milos Djordjevic <milosdjor011@gmail.com>
2020-04-07 15:08:28 -0500

    minor bugs fixed

--- container/Post.js
+++ container/Post.js
@@ -9,7 +9,7 @@ import {
   LayoutAnimation,
   FlatList,
   ScrollView,
-  RefreshControl
+  RefreshControl,
 } from "react-native";
 
 import Fire from "../Firebase";
@@ -17,22 +17,27 @@ import { connect } from "react-redux";
 import { Ionicons } from "@expo/vector-icons";
 import moment from "moment";
 
-const Post = props => {
-
+const Post = (props) => {
   console.log("props in post is: ", props);
   let uid = "1";
 
   const [liked, setLiked] = useState(false);
   const { post } = props;
   const { data } = props.post;
-
+  const [avatar, setAvatar] = useState("")
   if (data.uid === props.user.uid) {
     uid = null;
   }
-
+  const addAvatar = () => {
+    Fire.getAvatar(data.uid).then(item => {
+      setAvatar(item);
+      console.log("avatar prr", avatar);
+    });
+  };
   useEffect(() => {
+    addAvatar();
     if (data.likes) {
-      data.likes.map(like => {
+      data.likes.map((like) => {
         if (like.uid === Fire.uid) {
           setLiked(true);
         }
@@ -49,33 +54,38 @@ const Post = props => {
       Fire.addLike(post.id);
     }
   };
+  
   const heartIconColor = liked ? "rgb(220,100, 110)" : null;
 
   const heartIconSize = liked ? 40 : 39.5;
 
   return (
     <View style={styles.feedItem}>
-      <Image source={{ uri: data.avatar }} style={styles.avatar}></Image>
+      <Image source={{ uri: avatar }} style={styles.avatar} />
       <View style={{ flex: 1 }}>
         <View
           style={{
             flexDirection: "row",
             justifyContent: "space-between",
-            alignItems: "center"
+            alignItems: "center",
           }}
         >
           <View>
             {uid ? (
               <TouchableOpacity
-                onPress={() => props.navigation.navigate("Profile", {
-                  screen: "Friend Profile",
-                  params: { frienduid: data.uid, request: true }
-                })}
+                onPress={() =>
+                  props.navigation.navigate("Profile", {
+                    screen: "Friend Profile",
+                    params: { frienduid: data.uid, request: true },
+                  })
+                }
               >
                 <Text style={styles.post}>{data.name}</Text>
               </TouchableOpacity>
             ) : (
-              <TouchableOpacity onPress={() => props.navigation.navigate("Profile")}>
+              <TouchableOpacity
+                onPress={() => props.navigation.navigate("Profile")}
+              >
                 <Text style={styles.post}>{data.name}</Text>
               </TouchableOpacity>
             )}
@@ -104,7 +114,7 @@ const Post = props => {
               color="#737888"
               style={{
                 marginRight: 16,
-                color: heartIconColor
+                color: heartIconColor,
               }}
             />
           </TouchableOpacity>
@@ -123,7 +133,7 @@ const Post = props => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#EFECF4"
+    backgroundColor: "#EFECF4",
   },
   header: {
     backgroundColor: "#FFF",
@@ -135,50 +145,52 @@ const styles = StyleSheet.create({
     shadowOffset: { height: 5 },
     shadowRadius: 15,
     shadowOpacity: 0.2,
-    zIndex: 0
+    zIndex: 0,
   },
   feed: {
-    marginHorizontal: 16
+    marginHorizontal: 16,
   },
   feedItem: {
     backgroundColor: "#FFF",
     borderRadius: 5,
     padding: 8,
     flexDirection: "row",
-    marginVertical: 8
+    marginVertical: 8,
   },
   avatar: {
-    width: 36,
-    height: 36,
-    borderRadius: 18,
-    marginRight: 16
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 16,
+    marginTop: 6,
+    marginLeft: 5
   },
   name: {
     fontSize: 15,
     fontWeight: "500",
-    color: "#454D65"
+    color: "#454D65",
   },
   timestamp: {
     fontSize: 11,
     color: "#C4C6CE",
-    marginTop: 4
+    marginTop: 4,
   },
   post: {
     marginTop: 16,
     fontSize: 14,
-    color: "#838899"
+    color: "#838899",
   },
   postImage: {
     width: undefined,
     height: 150,
     borderRadius: 5,
     marginVertical: 16,
-    marginRight: 25
-  }
+    marginRight: 25,
+  },
 });
 
-const mapState = state => ({
-  user: state.user
+const mapState = (state) => ({
+  user: state.user,
 });
 
 export default connect(mapState)(Post);
